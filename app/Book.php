c98e06f4dd4b34227956fb50eae4a57a58fe2c12
Markus <m.babiano0823@gmail.com>
2020-05-24 22:17:11 +0800

    Implement checkout and checkin functionality

--- app/Book.php
+++ app/Book.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -18,10 +19,41 @@ class Book extends Model
         ]))->id;
         
     }
+
+    public function checkout(User $user)
+    {
+        $this->reservations()->create([
+            'book_id' => $this->id,
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin(User $user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->where('book_id', $this->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+        
+        if(is_null($reservation)){
+            throw new Exception();
+        }
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);    
+    }
     
 
     public function path()
     {
         return '/books/' . $this->id . '-'. Str::slug($this->title);
+    }
+
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }   
---
+++ app/Reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    protected $fillable = ['book_id','user_id','checked_out_at','checked_in_at'];
+}
